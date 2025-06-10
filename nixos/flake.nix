bd91b054ed8b70f803dcc2f830649a4e45fc3d87
Ivan <hi@crescentro.se>
2025-06-10 22:41:26 +0200

    chore: update system

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,7 +15,7 @@
     };
 
     quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      url = "git+https://git.outfoxxed.me/quickshell/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

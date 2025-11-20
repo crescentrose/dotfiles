7b615e42730c3bfc39c811a94dfefa16ca8a1144
Ivan <hi@crescentro.se>
2025-11-20 21:43:00 +0100

    chore: update system

--- home.nix
+++ home.nix
@@ -34,6 +34,56 @@
     walker = {
       enable = true;
       runAsService = true;
+      config = {
+        providers = {
+          default = [
+            "desktopapplications"
+            "calc"
+            "websearch"
+            "bookmarks"
+            "todo"
+          ];
+
+          prefixes = [
+            {
+              prefix = ";";
+              provider = "providerlist";
+            }
+            {
+              prefix = ">";
+              provider = "runner";
+            }
+            {
+              prefix = ".";
+              provider = "symbols";
+            }
+            {
+              prefix = "!";
+              provider = "todo";
+            }
+            {
+              prefix = "%";
+              provider = "bookmarks";
+            }
+            {
+              prefix = "@";
+              provider = "websearch";
+            }
+            {
+              prefix = ":";
+              provider = "clipboard";
+            }
+            {
+              prefix = "$";
+              provider = "windows";
+            }
+            {
+              prefix = "'";
+              provider = "nirisessions";
+            }
+          ];
+        };
+      };
     };
   };
 
@@ -100,7 +150,6 @@
 
         # developer tools
         gcc # the GNU Compiler Collection
-        hotspot # perf
         lldb # debugger
         rustup # rust installer
         strace # peek under the hood

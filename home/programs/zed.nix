bb23aebea7c63a0f1858aac573680170f621f7b6
Ivan <hi@crescentro.se>
2026-01-07 19:38:23 +0100

    feat: add Zed config

--- home/programs/zed.nix
+++ home/programs/zed.nix
@@ -1,6 +1,44 @@
 {
   programs.zed-editor = {
     enable = true;
+    extensions = [
+      # Theming
+      "catppuccin"
+      "catppuccin-icons"
 
+      # Language support
+      "toml"
+      "dockerfile"
+      "sql"
+      "make"
+      "terraform"
+      "nix"
+      "proto"
+    ];
+
+    userSettings = {
+      "terminal" = {
+        "font_family" = "Iosevka";
+        "shell" = {
+          "program" = "nu";
+        };
+      };
+      "buffer_line_height" = "comfortable";
+      "buffer_font_family" = "Iosevka";
+      "show_edit_predictions" = false;
+      "base_keymap" = "VSCode";
+      "toolbar" = {
+        "code_actions" = true;
+      };
+      "relative_line_numbers" = "enabled";
+      "helix_mode" = true;
+      "ui_font_size" = 15;
+      "buffer_font_size" = 14;
+      "theme" = {
+        "mode" = "system";
+        "light" = "Catppuccin Latte";
+        "dark" = "Catppuccin Macchiato";
+      };
+    };
   };
 }

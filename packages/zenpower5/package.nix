cc41ea5184ad765dcecd9d0f42eb1c66948060d9
Ivan <hi@crescentro.se>
2026-01-12 09:16:33 +0100

    chore: update zenpower5 kernel module

--- packages/zenpower5/package.nix
+++ packages/zenpower5/package.nix
@@ -7,24 +7,14 @@
 
 stdenv.mkDerivation rec {
   pname = "zenpower5-${version}-module-${kernel.modDirVersion}";
-  version = "0.5.0-patched";
+  version = "0.5.0-main";
 
-  # Use the branch that supports Granite Ridge
-  # see: https://github.com/mattkeenan/zenpower5/pull/3
   src = fetchFromGitHub {
-    owner = "SamG05Z";
+    owner = "mattkeenan";
     repo = "zenpower5";
-    rev = "feature/zen5-granite-ridge-support";
-    hash = "sha256-T+Dh6jAcF/7zbVGTo9ucmm4gVrNTP2vB7giTup7JdMI=";
+    rev = "66871d8"; # latest commit to `master`
+    hash = "sha256-g0zVTDi5owa6XfQN8vlFwGX+gpRIg+5q1F4EuxAk9Sk=";
   };
-  #
-  # TODO: use the main branch once merged
-  # src = fetchFromGitHub {
-  #   owner = "mattkeenan";
-  #   repo = "zenpower5";
-  #   rev = "v${version}";
-  #   hash = lib.fakeHash; # TODO: hash
-  # };
 
   hardeningDisable = [
     "pic"
